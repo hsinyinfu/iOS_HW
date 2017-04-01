75463745267fa99a96ce6563a6eb27681d47bcf6
hsinyinfu <peter0930260535@gmail.com>
2017-04-01 19:05:16 +0800

    完成HW2的Botton和Label. 	new file:   Caluculator/Caluculator/ViewController.swift

---
+++ Caluculator/Caluculator/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Caluculator
+//
+//  Created by 傅信穎 on 2017/4/1.
+//  Copyright © 2017年 傅信穎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
