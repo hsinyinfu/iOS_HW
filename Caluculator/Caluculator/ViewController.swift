ae0ffb9aa5206e5fa57af15b83f41333d738b3d5
hsinyinfu <peter0930260535@gmail.com>
2017-04-17 17:39:27 +0800

    完成計算機的數字按鈕、小數點、AC、+/-的功能

--- Caluculator/Caluculator/ViewController.swift
+++ Caluculator/Caluculator/ViewController.swift
@@ -9,5 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentNum = "0" {
+        didSet{
+            self.displayLabel.text = self.currentNum
+        }
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func numBtnClicked(_ sender: UIButton) {
+        let numBtnDigit = sender.tag - 1000
+        let btnText = "\(numBtnDigit)"
+        var curText : String = ""
+        if currentNum[ currentNum.startIndex ] == "-" {
+            curText = currentNum.substring(from: currentNum.index(after: currentNum.startIndex))
+        } else { curText = currentNum }
+        if curText == "0" {
+            guard btnText != "0" else {
+                return
+            }
+            curText = btnText
+        } else {
+            curText += btnText
+        }
+        if currentNum[ currentNum.startIndex ] == "-" {
+            self.currentNum = "-" + curText
+        }
+        else {
+            self.currentNum = curText
+        }
+    }
+
+    
+    @IBAction func AC_btnClicked(_ sender: UIButton) {
+        self.currentNum = "0"
+    }
+    
+    @IBAction func pos_negBtnClicked(_ sender: UIButton) {
+        if currentNum[ currentNum.startIndex ] == "-" {
+            currentNum.remove(at: currentNum.startIndex)
+        } else {
+            currentNum.insert("-", at: currentNum.startIndex)
+        }
+    }
+    
+    @IBAction func dotBtnClicked(_ sender: UIButton) {
+        guard !self.currentNum.contains(".") else {
+            return
+        }
+        self.currentNum +=  "."
+    }
+    
+    @IBAction func binaryOperatorBtnClicked(_ sender: UIButton) {
+    }
+
+    @IBAction func logBtnClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func expBtnClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func piBtnClicked(_ sender: UIButton) {
+        self.currentNum = "\(Double.pi)"
+    }
+    
+    @IBAction func equalBtnClicked(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
